aaf0932c2af91d0fe89cad10f316dcacb3229633
Alex Froebel <afroebel@caltech.edu>
2016-09-13 14:30:44 -0700

    second part of plotting lines now included, first part should be separate

--- plotting_lines.py
+++ plotting_lines.py
@@ -11,6 +11,17 @@ norm_I = 4 * (scipy.special.j1(x) / x)**2
 
 # Plot our computation
 # $$ makes stuff in between be rendered more mathishy
+plt.close
 plt.plot(x, norm_I, marker='.', linestyle='none')
 plt.xlabel('$x$')
 plt.ylabel('$I(x) / I_0$')
+
+# Processing the spike data.
+data = np.loadtxt('data/retina_spikes.csv', skiprows=2, delimiter=',')
+t = data[:,0]
+V = data[:,1]
+
+plt.close()
+plt.plot(t, V)
+plt.xlabel('t (ms)')
+plt.ylabel('V (uV)')
